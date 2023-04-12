5f1673b8f821cbc467b0be05fe792f88742eb45c
Yoojin93 <yoojinbae93@gmail.com>
2023-04-11 22:42:16 -0500

    updated

---
+++ api/whoishiring/companies/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from api.whoishiring.companies.models import Company
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ["id", "name", "status", "application_link", "last_update", "notes"]
