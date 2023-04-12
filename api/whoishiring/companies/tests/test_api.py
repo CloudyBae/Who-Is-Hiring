f414e33303debba859a08a3b1f64fe6bcaeb9177
Yoojin93 <yoojinbae93@gmail.com>
2023-04-12 11:45:41 -0500

    pytest tests

---
+++ api/whoishiring/companies/tests/test_api.py
@@ -0,0 +1,54 @@
+import json
+from unittest import TestCase
+import pytest
+from django.urls import reverse
+from django.test import Client
+from api.whoishiring.companies.models import Company
+
+@pytest.mark.django_db
+class BasicCompanyApiTestCase(TestCase):
+    def setUp(self)->None:
+        self.client = Client()
+        self.companies_url = reverse("companies-list")
+
+    def tearDown(self)->None:
+        pass
+
+
+class TestGetCompanies(BasicCompanyApiTestCase):
+    def test_zero_companies_should_return_empty_list(self)->None:
+        response = self.client.get(self.companies_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content), [])
+
+    def test_one_company_exists_should_succeed(self)->None:
+        test_company = Company.objects.create(name="Amazon")
+        response = self.client.get(self.companies_url)
+        response_content = json.loads(response.content)[0]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_content.get("name"), test_company.name)
+        self.assertEqual(response_content.get("status"), "Hiring")
+        self.assertEqual(response_content.get("application_link"), "")
+        self.assertEqual(response_content.get("notes"), "")
+
+        test_company.delete()
+
+class TestPostCompanies(BasicCompanyApiTestCase):
+    def test_create_company_without_arguments_should_fail(self)->None:
+        response = self.client.post(path=self.companies_url)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            json.loads(response.content), {"name": ["This field is required."]}
+        )
+
+    def test_create_existing_company_should_fail(self)->None:
+      Company.objects.create(name="apple")
+      response = self.client.post(path=self.companies_url, data={"name":"apple"})
+      self.assertEqual(response.status_code, 400)
+      self.assertEqual(
+          json.loads(response.content),
+          {"name": ["company with this name already exists."]},
+      )
+
+    def test_create_company_with_only_name_all_fields_should_be_default(self)->None:
+        pass
