76725f857add1a718310ea70a8b506617644e71d
Yoojin93 <yoojinbae93@gmail.com>
2023-04-14 12:07:39 -0500

    update

--- api/whoishiring/whoishiring/settings.py
+++ api/whoishiring/whoishiring/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "api.whoishiring.companies",
-    "api.whoishiring.whoishiring"
+    "api.whoishiring.whoishiring",
+    "api.whoishiring.companies.models"
 ]
 
 MIDDLEWARE = [
